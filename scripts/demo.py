7d59ec74ae894929667fba5257054e02dbe3516c
Kai Riedmiller <kai.riedmiller@iwr.uni-heidelberg.de>
2026-01-19 14:52:33 +0100

    update paths

--- scripts/demo.py
+++ scripts/demo.py
@@ -22,9 +22,9 @@ viridis = mpl.colormaps.get("viridis")
 # %%
 project_path = Path.cwd() / ".." / "example_analysis"
 p = Project(project_path, compression_level="s2", loglevel="DEBUG", clipping=((0.4,0.4,0.4),(0.6,0.6,0.6)))
-p.add_source("../data/Interphase_4T/mito_it00_b0_7_stitched.xml", "mito")
+p.add_source("../data/old_cebraEM/Interphase_4T/mito_it00_b0_7_stitched.xml", "mito")
 s = p.sources["mito_it00_b0_7_stitched"]
-p.add_source("../data/Interphase_4T/er_it00_b0_7_stitched.xml", "er")
+p.add_source("../data/old_cebraEM/Interphase_4T/er_it00_b0_7_stitched.xml", "er")
 s = p.sources["er_it00_b0_7_stitched"]
 
 # %%
