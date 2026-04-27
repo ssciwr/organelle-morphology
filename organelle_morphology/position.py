fix: padding calculation for position analysis

organelle_morphology/position.py
@@ -10,6 +10,7 @@ import logging
 import dask.array as da
 
 from organelle_morphology.statistics import Properties, Stats
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +29,18 @@ class PositionMeta(Properties):
 @dataclass
 class PositionProperties(Properties):
     density: np.ndarray
+
+    def get_plot(self):
+        if len(self.density.shape) == 3:
+            raise NotImplementedError()
+        elif len(self.density.shape) == 2:
+            return plt.imshow(self.density)
+        elif len(self.density.shape) == 1:
+            return plt.plot(self.density)
+
+    def plot(self):
+        self.get_plot()
+        plt.show()
 
 
 class Position_Analysis(Analysis):
@@ -62,7 +75,7 @@ class Position_Analysis(Analysis):
         cache_key = f"density3d_{res[0]:.6f}-{res[1]:.6f}-{res[2]:.6f}"
         if cache_key not in source.cache:
             binsize = (np.array(bin_resolution) // source.resolution).astype(int)
-            n_missing = source.data.shape % binsize
+            n_missing = (binsize - (source.data.shape % binsize)) % binsize
 
             data = source.data.astype(bool)
             data = da.pad(data, [(0, i) for i in n_missing])

tests/test_position_analysis.py
@@ -33,17 +33,17 @@ def test_density2D(project_with_sources):
     res0 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 2, 0.0, (0, 1))
     res1 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 2, 10, (0, 1))
     res2 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 2, 20, (0, 1))
-    res3 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 1, 0.0, (0, 1))
-    res4 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 1, 10, (2, 1))
-    res5 = posan.density2D(p.sources["synth_data"], (2, 3, 4), 1, 20, (2, 1))
+    res3 = posan.density2D(p.sources["synth_data"], (7, 3, 5), 1, 0.0, (0, 1))
+    res4 = posan.density2D(p.sources["synth_data"], (7, 3, 5), 1, 10, (2, 1))
+    res5 = posan.density2D(p.sources["synth_data"], (7, 3, 5), 1, 20, (2, 1))
 
     np.testing.assert_almost_equal(res0.mean(), 0.0391694957797291, 5)
     assert res0.shape == (118, 80)
     assert res1.shape == (130, 99)
     assert res2.shape == (138, 116)
-    assert res3.shape == (118, 83)
-    assert res4.shape == (118, 96)
-    assert res5.shape == (118, 105)
+    assert res3.shape == (34, 66)
+    assert res4.shape == (34, 79)
+    assert res5.shape == (34, 89)
 
     # import matplotlib.pyplot as plt
     # fig, axes = plt.subplots(2, 3, figsize=(12, 8))